940b75d09f32ca0391be09f396f2ca9bcf5603c9
Satoshi Deguchi <deguchik@gmail.com>
2015-10-09 15:28:52 +0900

    overflow-yに変更

--- src/searchReact.js
+++ src/searchReact.js
@@ -217,11 +217,11 @@ var SearchResult = React.createClass({
     },
     closeHandler: function () {
         $(this.refs.searchResult.getDOMNode()).fadeOut();
-        $('html,body').css('overflow', 'hidden');
+        $('html,body').css('overflow-y', 'hidden');
         this.stop();
     },
     render: function () {
-        $('html,body').css('overflow', 'visible');
+        $('html,body').css('overflow-y', 'visible');
         var messageNode = null;
         if (this.props.target.message) {
             messageNode = React.createElement("p", {className: "searchMessage"}, this.props.target.message)
