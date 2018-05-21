6a9c004e18b4101236a37793b47d63b36c513ca0
Satoshi Deguchi <deguchik@gmail.com>
2018-05-21 15:07:20 +0900

    update

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,60 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "com.unarin.cordova.beacon.underscorejs",
+    "file": "plugins/com.unarin.cordova.beacon/www/lib/underscore-min-1.6.js",
+    "pluginId": "com.unarin.cordova.beacon",
+    "runs": true
+  },
+  {
+    "id": "com.unarin.cordova.beacon.Q",
+    "file": "plugins/com.unarin.cordova.beacon/www/lib/q.min.js",
+    "pluginId": "com.unarin.cordova.beacon",
+    "runs": true
+  },
+  {
+    "id": "com.unarin.cordova.beacon.LocationManager",
+    "file": "plugins/com.unarin.cordova.beacon/www/LocationManager.js",
+    "pluginId": "com.unarin.cordova.beacon",
+    "merges": [
+      "cordova.plugins"
+    ]
+  },
+  {
+    "id": "com.unarin.cordova.beacon.Delegate",
+    "file": "plugins/com.unarin.cordova.beacon/www/Delegate.js",
+    "pluginId": "com.unarin.cordova.beacon",
+    "runs": true
+  },
+  {
+    "id": "com.unarin.cordova.beacon.Region",
+    "file": "plugins/com.unarin.cordova.beacon/www/model/Region.js",
+    "pluginId": "com.unarin.cordova.beacon",
+    "runs": true
+  },
+  {
+    "id": "com.unarin.cordova.beacon.Regions",
+    "file": "plugins/com.unarin.cordova.beacon/www/Regions.js",
+    "pluginId": "com.unarin.cordova.beacon",
+    "runs": true
+  },
+  {
+    "id": "com.unarin.cordova.beacon.CircularRegion",
+    "file": "plugins/com.unarin.cordova.beacon/www/model/CircularRegion.js",
+    "pluginId": "com.unarin.cordova.beacon",
+    "runs": true
+  },
+  {
+    "id": "com.unarin.cordova.beacon.BeaconRegion",
+    "file": "plugins/com.unarin.cordova.beacon/www/model/BeaconRegion.js",
+    "pluginId": "com.unarin.cordova.beacon",
+    "runs": true
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "com.unarin.cordova.beacon": "3.3.0"
+};
+// BOTTOM OF METADATA
+});
