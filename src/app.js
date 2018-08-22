56c03204fc9df4ab6478cbfa0437b38a07e5965a
deguchi <deguchik@gmail.com>
2018-08-22 15:54:13 +0900

    ファイルの整理

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ol from 'openlayers';
 import Kanikama from './libs/kanikama.js';
 import Kanimarker from './libs/kanimarker.js';
 import Kanilayer from './libs/kanilayer.js';
-import InitUI from './searchReact.jsx';
+import InitUI from './InitUI.jsx';
 
 var MAPBOX_TOKEN = "pk.eyJ1IjoiY2FsaWxqcCIsImEiOiJxZmNyWmdFIn0.hgdNoXE7D6i7SrEo6niG0w";
 
