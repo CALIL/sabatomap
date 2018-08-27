236ee3c0736eebd84e2d38379294aff71c0e9d5d
deguchi <deguchik@gmail.com>
2018-08-27 10:17:56 +0900

    fix load

--- src/load.js
+++ src/load.js
@@ -6,16 +6,16 @@ function load() {
     window.app.initializeApp();
   };
 
-  request.get('https://calil.jp/static/apps/sabatomap/update_v160.js?_=' + Math.random())
-    .timeout(5000)
-    .end(function (err, res) {
-      if (err || !res.ok) {
-        script.src = 'js/all.js';
-      } else {
-        script.innerHTML = res.text;
-      }
-      document.body.appendChild(script);
-    });
+  // request.get('https://calil.jp/static/apps/sabatomap/update_v160.js?_=' + Math.random())
+  //   .timeout(5000)
+  //   .end(function (err, res) {
+  //     if (err || !res.ok) {
+  //       script.src = 'js/all.js';
+  //     } else {
+  //       script.innerHTML = res.text;
+  //     }
+  //     document.body.appendChild(script);
+  //   });
 }
 
 if (typeof cordova !== "undefined" && cordova !== null) {
