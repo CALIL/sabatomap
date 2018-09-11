6a514ee98b5819bcaed7fc593c911557b0f14382
deguchi <deguchik@gmail.com>
2018-09-11 14:35:44 +0900

    参考URLを追加

--- android_build.bat
+++ android_build.bat
@@ -1,5 +1,7 @@
 cordova build android --release
 
+REM https://qiita.com/nogson/items/abe1016f36c3b331db30
+REM https://stackoverflow.com/questions/44874386/apk-upload-failed-for-alpha-version-errors-from-apksigner-digest-algorithm-and
 jarsigner -verbose -tsa http://timestamp.digicert.com -sigalg SHA1withRSA -digestalg SHA1 -keystore ./calil.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk calil
 jarsigner -verify -verbose -certs ./platforms/android/build/outputs/apk/android-release-unsigned.apk
 
