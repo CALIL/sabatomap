52fd22fb748e5696b32c7a59e9fd2b31e8ec5aba
deguchi <deguchik@gmail.com>
2018-10-02 14:23:55 +0900

    add hint

--- android_build.bat
+++ android_build.bat
@@ -2,7 +2,8 @@ cordova build android --release
 
 REM https://qiita.com/nogson/items/abe1016f36c3b331db30
 REM https://stackoverflow.com/questions/44874386/apk-upload-failed-for-alpha-version-errors-from-apksigner-digest-algorithm-and
-jarsigner -verbose -tsa http://timestamp.digicert.com -sigalg SHA1withRSA -digestalg SHA1 -keystore ./calil.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk calil
-jarsigner -verify -verbose -certs ./platforms/android/build/outputs/apk/android-release-unsigned.apk
+REM keystore pass keepass
+jarsigner -verbose -tsa http://timestamp.digicert.com -sigalg SHA1withRSA -digestalg SHA1 -keystore ./calil.keystore ./platforms/android/build/outputs/apk/release/android-release-unsigned.apk calil
+jarsigner -verify -verbose -certs ./platforms/android/build/outputs/apk/release/android-release-unsigned.apk
 
-zipalign -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk android-release.apk
+zipalign -v 4 ./platforms/android/build/outputs/apk/release/android-release-unsigned.apk android-release.apk
