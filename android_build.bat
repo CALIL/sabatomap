9e9bde5f87e846739cf7118875fcf93fe05051d8
deguchi <deguchik@gmail.com>
2018-11-26 15:11:41 +0900

    パスの変更対応

--- android_build.bat
+++ android_build.bat
@@ -3,7 +3,7 @@ cordova build android --release
 REM https://qiita.com/nogson/items/abe1016f36c3b331db30
 REM https://stackoverflow.com/questions/44874386/apk-upload-failed-for-alpha-version-errors-from-apksigner-digest-algorithm-and
 REM keystore pass keepass
-jarsigner -verbose -tsa http://timestamp.digicert.com -sigalg SHA1withRSA -digestalg SHA1 -keystore ./calil.keystore ./platforms/android/build/outputs/apk/release/android-release-unsigned.apk calil
-jarsigner -verify -verbose -certs ./platforms/android/build/outputs/apk/release/android-release-unsigned.apk
+jarsigner -verbose -tsa http://timestamp.digicert.com -sigalg SHA1withRSA -digestalg SHA1 -keystore ./calil.keystore ./platforms\android\build\outputs\apk\android-release-unsigned.apk calil
+jarsigner -verify -verbose -certs ./platforms\android\build\outputs\apk\android-release-unsigned.apk
 
-zipalign -v 4 ./platforms/android/build/outputs/apk/release/android-release-unsigned.apk android-release.apk
+zipalign -v 4 .\platforms\android\build\outputs\apk\android-release-unsigned.apk android-release.apk
